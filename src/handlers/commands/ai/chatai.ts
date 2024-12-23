6fed563cafb6116f01d544c79106fef77e9361a0
Harukoto <128772893+Harukotokoto@users.noreply.github.com>
2024-12-23 17:04:15 +0900

    Update chatai.ts

--- src/handlers/commands/ai/chatai.ts
+++ src/handlers/commands/ai/chatai.ts
@@ -1,5 +1,5 @@
 import { Command } from '@/handlers/Command';
-import { APIEmbed, ApplicationCommandOptionType, Colors } from 'discord.js';
+import { APIEmbed, ApplicationCommandOptionType, Colors, ApplicationIntegrationType, InteractionContextType } from 'discord.js';
 import { ChatAI } from '@/libraries/Classes/Modules/ChatAI';
 import { ModelCategories } from '@/libraries/Enums/ModelCategories';
 import { CommandError } from '@/handlers/CommandError';
