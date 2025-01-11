a58a6ae3d73d2b4d2875f68fab172e4349bf93b5
Harukotokoto <y.games@outlook.jp>
2025-01-11 18:58:17 +0900

    .gitignoreを更新

--- dist/libraries/Enums/AIRoles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AIRoles = void 0;
-var AIRoles;
-(function (AIRoles) {
-    AIRoles["System"] = "system";
-    AIRoles["User"] = "user";
-    AIRoles["Assistant"] = "assistant";
-})(AIRoles || (exports.AIRoles = AIRoles = {}));
--- dist/libraries/Functions/truncateText.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.truncateText = truncateText;
-function truncateText(text, maxLength = 49) {
-    return text.length > maxLength ? text.slice(0, maxLength) + '…' : text;
-}
